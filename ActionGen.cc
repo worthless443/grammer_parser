ee9d3923641007367b89924856532d416fa56179
worthless443 <aissypewdiepie@gmail.com>
2022-09-28 23:49:49 +0530

    new action table creation method

--- ActionGen.cc
+++ ActionGen.cc
@@ -9,17 +9,16 @@
 #include<regex>
 #include <initializer_list>
 
-
 #include<cstring>
 #include<cstdlib>
 
 #include<parser_prot.h>
-//#include<decorate_error.h>
 #include<eval.h>
 #include<stack.h>
 #include<TableGen.h>
 #include<ActionGen.h>
 
+// there should be a table 
 ActionsEnum action(Stack<int> stack, int word) {
 	if(stack.top() ==  10) {
 		return Accept;
@@ -27,6 +26,37 @@ ActionsEnum action(Stack<int> stack, int word) {
 	if(stack.top()==word) return Reduce;
 	else if(stack.top()!=word) return Shift;
 	else return Fail;
+}
+
+ActionsEnum action_(std::vector<Item> items) {
+	for(Item item : items) {
+		if(item.getPlaceHolder() == 2) return Accept;
+		else if(item.getPlaceHolder() == 1) return Shift;
+		else if(item.getPlaceHolder() == 0) return Reduce;
+		else return Fail;
+	}
+	return Fail;
+}
+
+ActionsEnum mapEnum(Item item) {
+	if(item.getPlaceHolder() == 2) return Accept;
+	else if(item.getPlaceHolder() == 1) return Reduce;
+	else if(item.getPlaceHolder() == 0) return Shift;
+	else return Fail;
+}
+
+ActionTable ActionGen(std::vector<std::vector<Item>> items2d) {
+	ActionTable actions;
+	int idx = 0;
+	for(auto vec : items2d) {
+		for(Item item : vec) {
+			if(item.getPlaceHolder()==0) actions[idx] = Shift;
+			if(item.getPlaceHolder()==1) actions[idx] = Reduce ;
+			if(item.getPlaceHolder()==2) actions[idx] = Accept ;
+			idx++;
+		}
+	}
+	return actions;
 }
 
 #ifdef __main__ 
--- ActionGen.h
+++ ActionGen.h
@@ -1,9 +1,30 @@
+
 enum ActionsEnum {
 	Shift,Reduce, Accept, Fail
 };
 ActionsEnum action(Stack<int> stack, int word);
+ActionsEnum action_(std::vector<Item>);
+ActionsEnum mapEnum(Item item);
+class ActionTable {
+	int lookahead, idx, action;
+	std::vector<std::pair<int,int>> cor;
+	using T = std::vector<std::pair<int,int>>;
+public:
+	T::iterator begin() {return cor.begin();}
+	T::iterator end() {return cor.end();}
+	size_t size() {return cor.size();}
+	ActionTable(int lookahead, int idx, int action) : lookahead(lookahead), action(action), idx(idx) {}
+	ActionTable() = default;
+	int &operator=(int lookahead) {cor.push_back(std::make_pair(cor.size(), lookahead));return cor[cor.size()-1].second;}
+	ActionTable &operator[](int lookahead) {
+		return *this;
+	}
+};
+
 class skLr {
 	std::vector<int> vec;
+	std::vector<std::vector<Item>> items2d;
+	Stack<int> stack;
 public:
 	skLr(std::initializer_list<int> l)  : vec{l} {
 		vec.push_back(10); // eof
@@ -13,14 +34,43 @@ public:
 		vec.push_back(10);
 		init();
 	}
+	skLr(std::vector<std::vector<Item>> items2d) : items2d{items2d} { }
+	const bool reduction_shifting() { // to see if they are homomorphic (and they will be)
+		int reduction = 0, shift=0, fail=0;
+		for(auto vec_ : items2d) {
+			while(1)  {
+				if(action_(vec_)==Reduce) {
+					++reduction;
+				}
+				if(action_(vec_)==Shift) {
+					++shift;
+				}
+				if(action_(vec_)==Fail) ++fail;
+				if(action_(vec_)==Accept) break;
+
+				vec_ = Iter2VecT<Item>(vec_.begin()+1, vec_.end());
+			}
+		}
+		return reduction == shift;
+	}
+	void test() {
+		for (auto vec : items2d) {
+			while(1) {
+			 if(mapEnum(vec[0])==Reduce) std::cout << "Reduce\n"; //reduce
+			 if(mapEnum(vec[0])==Shift) std::cout << "Shift\n"; //reduce
+			 if(mapEnum(vec[0])==Accept) break ; //reduce
+			 vec = Iter2VecT<Item>(vec.begin()+1, vec.end());
+			}
+			std::cout << "\n";
+		}
+	}
+private:
 	void init() {
-		Stack<int> stack;
 		int word;
 		stack.push(0);
 		int i=0;
 		ActionsEnum actions;
 		while(1) {
-			std::cout << "nigger\n";
 			if(action(stack, word)==Reduce) {
 				stack.pout(word);
 				word = vec[i];
@@ -35,3 +85,4 @@ public:
 	}
 };
 
+ActionTable ActionGen(std::vector<std::vector<Item>> items2d);
